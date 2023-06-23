ce27b54bed1d19ed8c8820076950dbadda8d2bab
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-23 10:17:25 +0545

    added css to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import './ExpenseItem.css'
 function ExpenseItem() {
-    return(
-    <h2>Expense item!
-    </h2>
-    )
+  return (
+    <div className="expense-item">
+      <div className="expense-item__description" >March 28th 2021</div>
+      <div className="expense-item__price">
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
-export default ExpenseItem
+export default ExpenseItem;
