e07c561ff53c8af9006fe65d60b9daeaa3d175be
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-23 16:56:22 +0545

    code reuseubility and props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,11 @@
-import './ExpenseItem.css'
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div className="expense-item__description" >March 28th 2021</div>
-      <div className="expense-item__price">
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
