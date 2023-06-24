ddff3a17e85bdbf0035bb0a43b09365b1ceba1ce
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-24 23:57:55 +0545

    assignment1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate"
-function ExpenseItem(props) {
- 
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
      <ExpenseDate date={props.date}/>
@@ -12,4 +11,4 @@ function ExpenseItem(props) {
     </div>
   );
 }
-export default ExpenseItem;
+
