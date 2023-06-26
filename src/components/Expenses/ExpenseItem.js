99e932cb065c5201e43967fa4706fcbfed72fd02
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-26 23:59:23 +0545

    useState hook in React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,23 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate"
+import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import '../UI/Card.css'
- const ExpenseItem=(props)=> {
-  const eventHandler=()=>{
-    console.log('clicked!!')
-  }
+import "../UI/Card.css";
+
+const ExpenseItem = (props) => {
+  const [title,setTitle]=useState(props.title)
+  const eventHandler = () => {
+    setTitle('updated')
+  };
   return (
-  <Card className="expense-item">
-     <ExpenseDate date={props.date}/>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={eventHandler}>click Here</button>
-  </Card>
+    </Card>
   );
-}
-export default ExpenseItem
+};
+export default ExpenseItem;
