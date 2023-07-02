fcb9138c9bee6fe5154b7acceacfbd53cb57a4b6
sushilbhatta <sushilbhatta58@gmail.com>
2023-07-02 23:17:01 +0545

    adding chart and styling it

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,19 +1,24 @@
-import ChartBar from "./ChartBar";
-import  './ChartBar.css'
+import React from 'react';
+
+import ChartBar from './ChartBar';
+import './Chart.css';
+
 const Chart = (props) => {
-    const dataPointValue=props.dataPoints.map((datapoint)=>datapoint)
-    const totalMaximum=Math.max(...dataPointValue)
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
-    <div className="chart">
-      {props.dataPoints.map((datapoint) => {
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
         <ChartBar
-          key={datapoint.label}
-          value={datapoint.value}
+          key={dataPoint.label}
+          value={dataPoint.value}
           maxValue={totalMaximum}
-          label={datapoint.label}
-        />;
-      })}
+          label={dataPoint.label}
+        />
+      ))}
     </div>
   );
 };
+
 export default Chart;
