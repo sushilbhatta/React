e1c4caa117544e3664cc1b83459c896b85da409b
sushilbhatta <sushilbhatta58@gmail.com>
2024-04-23 08:10:36 +0545

    image shorting

---
+++ React_p7_imagePicker/src/App.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useRef, useState } from "react";
+
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+
+import { sortPlacesByDistance } from "./loc.js";
+
+function App() {
+  const modal = useRef();
+  const selectedPlace = useRef();
+  const [availablePlaces, setAvaliablePlaces] = useState([]);
+  // picked places
+  const [pickedPlaces, setPickedPlaces] = useState([]);
+  // get current location of the current user
+  // getCurrentPosition may take some time to fetch the current location of the user so we'll use the callback function
+  // useEffect
+  // useeffect takes the two parameter  1st one is the function that handles the callback and the 2nd oone is the array of dependencies to handle the function
+  // if dependencies is not specified then it function will execute infinitly.
+  // the useeffect() function is executed after the component in which the useEffect() hook is executed is fully executed.
+  //the useeffect hooks is re-executed wheever the value of dependencies are changed  else it will not reexecute.
+  // we can simply put the empty array if we  want  useEffect() function to not run infinitely as empty array means that the dependencies value is not changed.
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const shortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvaliablePlaces(shortedPlaces);
+      // here when we trigger the render for App component and it is reexecuted which will reexecute the navigator to get the current location and again  it will  set the  state which will again execute the App component and navigator  function and again it will set the state variable and this process continues infinetly.
+      // so  handling sideeffect  has a very serious flaw by this process.so solution is to use the useEffect() hook provided by the react to handle the sideeffect.
+    });
+  }, []);
+
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   const shortedPlaces = sortPlacesByDistance(
+  //     AVAILABLE_PLACES,
+  //     position.coords.latitude,
+  //     position.coords.longitude
+  //   );
+  //   setAvaliablePlaces(shortedPlaces);
+  //   // here when we trigger the render for App component and it is reexecuted which will reexecute the navigator to get the current location and again  it will  set the  state which will again execute the App component and navigator  function and again it will set the state variable and this process continues infinetly.
+  //   // so  handling sideeffect  has a very serious flaw by this process.so solution is to use the useEffect() hook provided by the react to handle the sideeffect.
+  // });
+  // the avove navigator callback task is the sideeffect in the js because:
+  // 1. this  task is not directly releated to the  primaiary objective of the App() component as the main task of the App() component is to return the  jsx code and it should only consits of functions or code that would help in returning the jsx code like deleating the image rendering the image and so on .
+  // 2. the avove task is not performed instantly but it takes the time to get the current location of the user and is executed after the App() component is executed .
+
+  // now  we need to  use the shortedPlaes in the Places component in the jsx code below. but we donot get the shorted places in the first render of jsx so we need to trigger the event and use state to trigger the second render .
+  function handleStartRemovePlace(id) {
+    modal.current.open();
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modal.current.close();
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    modal.current.close();
+  }
+
+  return (
+    <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt='Stylized globe' />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={"Select the places you would like to visit below."}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title='Available Places'
+          places={availablePlaces}
+          fallbackText='Soting places by distances...'
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
--- React_p7_imagePicker/src/loc.js
+++ React_p7_imagePicker/src/loc.js
@@ -1,20 +1,17 @@
 function toRad(value) {
   return (value * Math.PI) / 180;
-  //convert the degreee to radian
-  //  360deg=2PI rad
-  // 1deg=2PI/360=PI/180 rad
 }
 
 function calculateDistance(lat1, lng1, lat2, lng2) {
-  const R = 6371; //radius of earth
-  const dLat = toRad(lat2 - lat1); //latitude difference between two points
-  const dLon = toRad(lng2 - lng1); //lngitude difference between two points
-  const l1 = toRad(lat1); // latitude
+  const R = 6371;
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lng2 - lng1);
+  const l1 = toRad(lat1);
   const l2 = toRad(lat2);
 
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(l1) * Math.cos(l2); // haversine formula two calculate the shortest distance between two points in the   sphere
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(l1) * Math.cos(l2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
   return d;
